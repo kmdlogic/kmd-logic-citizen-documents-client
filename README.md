e471cedcb0fefd32ae3b18bcf307afd195fae248
DU00556591 <DU00556591@INBASDLP06496>
2020-01-09 17:15:41 +0530

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-# kmd-logic-citizen-documents-client
+# KMD Logic CitizenDocuments Client
+
+A dotnet client library for uploading and sending documents for citizens via the Logic platform.
+
+## How to use this client library
+
+In projects or components where you need to upload or send documents, add a NuGet package reference to [Kmd.Logic.CitizenDocuments.Client](https://www.nuget.org/packages/Kmd.Logic.CitizenDocuments.Client).
+
+The simplest example to get a citizens details is:
+
+```csharp
+using (var httpClient = new HttpClient())
+{
+     var tokenProviderFactory = new LogicTokenProviderFactory(tokenProviderOptions);
+     var citizenDocumentClient = new CitizenDocumentsClient(httpClient, tokenProviderFactory, configuration.Citizen);
+     var uploadDocument = await citizenDocumentClient.UploadAttachmentWithHttpMessagesAsync(new Guid(configuration.SubscriptionId), configuration.ConfiguartionId, configuration.RetentionPeriodInDays, configuration.Cpr, configuration.DocumentType, configuration.Document, configuration.DocumentName).ConfigureAwait(false);
+     var sendDocument = await citizenDocumentClient.SendDocumentWithHttpMessagesAsync(new Guid(configuration.SubscriptionId), new SendCitizenDocumentRequest
+}
+```
+
+The `LogicTokenProviderFactory` authorizes access to the Logic platform through the use of a Logic Identity issued client credential. The authorization token is reused until it  expires. You would generally create a single instance of `LogicTokenProviderFactory`.
+
+The `CitizenDocumentsClient` accesses the Logic CitizenDocuments service which in turn interacts with one of the data providers.
+
+## How to configure the CitizenDocuments client
+
+Perhaps the easiest way to configure the CitizenDocuments client is from Application Settings.
+
+```json
+{
+  "TokenProvider": {
+    "ClientId": "",
+    "ClientSecret": "",
+    "AuthorizationScope": "https://logicidentityprod.onmicrosoft.com/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx/.default",
+    "AuthorizationTokenIssuer": "https://login.microsoftonline.com/logicidentityprod.onmicrosoft.com/oauth2/v2.0/token"
+  }
+}
+```
+
+To get started:
+
+1. Create a subscription in [Logic Console](https://console.kmdlogic.io). This will provide you the `SubscriptionId`.
+2. Request a client credential. Once issued you can view the `ClientId` and `ClientSecret` in [Logic Console](https://console.kmdlogic.io).
+3. Create a CitizenDocuments configuration. Select the Digital Post configuration and upload the details. This will give you the `CitizenDocumentsConfigurationId`.
+
+## Sample application
+
+A simple console application is included to demonstrate how to call Logic CitizenDocuments API. You will need to provide the settings described above in `appsettings.json`.
+
+When run you should see the details of the _FileAccessPage_ and _messageId_ is printed to the console.
+
+
--- src/Kmd.Logic.CitizenDocuments.Client/CitizenDocumentsClient.cs
+++ src/Kmd.Logic.CitizenDocuments.Client/CitizenDocumentsClient.cs
@@ -2,6 +2,7 @@
 using Kmd.Logic.Identity.Authorization;
 using Microsoft.Rest;
 using System;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Net.Http;
 using System.Text;
@@ -9,6 +10,17 @@ using System.Threading.Tasks;
 
 namespace Kmd.Logic.CitizenDocuments.Client
 {
+
+    /// <summary>
+    /// upload and send documents.
+    /// </summary>
+    /// <remarks>
+    /// To access the Citizen documents you:
+    /// - Create a Logic subscription
+    /// - Have a client credential issued for the Logic platform
+    /// - Create a Citizen document configuration for the distribution service being used.
+    /// </remarks>
+    [SuppressMessage("Design", "CA1001:Types that own disposable fields should be disposable", Justification = "HttpClient is not owned by this class.")]
     public sealed class CitizenDocumentsClient
     {
         private readonly HttpClient httpClient;
@@ -17,6 +29,12 @@ namespace Kmd.Logic.CitizenDocuments.Client
 
         private InternalClient internalClient;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CitizenDocumentClient"/> class.
+        /// </summary>
+        /// <param name="httpClient">The HTTP client to use. The caller is expected to manage this resource and it will not be disposed.</param>
+        /// <param name="tokenProviderFactory">The Logic access token provider factory.</param>
+        /// <param name="options">The required configuration options.</param>
         public CitizenDocumentsClient(HttpClient httpClient, LogicTokenProviderFactory tokenProviderFactory, CitizenDocumentsOptions options)
         {
             this.httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
@@ -24,6 +42,14 @@ namespace Kmd.Logic.CitizenDocuments.Client
             this.tokenProviderFactory = tokenProviderFactory ?? throw new ArgumentNullException(nameof(tokenProviderFactory));
         }
 
+        /// <summary>
+        /// Uploads the single citizen document.
+        /// </summary>
+        /// <returns>The fileaccess page details or error if isn't valid.</returns>
+        /// <exception cref="ValidationException">Missing cpr number.</exception>
+        /// <exception cref="SerializationException">Unable process the service response.</exception>
+        /// <exception cref="LogicTokenProviderException">Unable to issue an authorization token.</exception>
+        /// <exception cref="CitizenDocumentsException">Invalid Citizen documents configuration details.</exception>
         public async Task<CitizenDocumentUploadResponse> UploadAttachmentWithHttpMessagesAsync(Guid subscriptionId, string configurationId, int retentionPeriodInDays, string cpr, string documentType, Stream document, String documentName)
         {
             var client = this.CreateClient();
@@ -52,6 +78,14 @@ namespace Kmd.Logic.CitizenDocuments.Client
             }
         }
 
+        /// <summary>
+        ///  Sends the documents to citizens.
+        /// </summary>
+        /// <param name="sendCitizenDocumentRequest">The send request class.</param>
+        /// <returns>The messageId or error if the identifier isn't valid.</returns>
+        /// <exception cref="SerializationException">Unable process the service response.</exception>
+        /// <exception cref="LogicTokenProviderException">Unable to issue an authorization token.</exception>
+        /// <exception cref="CitizenDocumentsException">Invalid Citizen configuration details.</exception>
         public async Task<SendCitizenDocumentResponse> SendDocumentWithHttpMessagesAsync(Guid subscriptionId, SendCitizenDocumentRequest sendCitizenDocumentRequest)
         {
             var client = this.CreateClient();
